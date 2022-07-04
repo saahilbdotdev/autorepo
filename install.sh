57c68ca8965d1fe2d34a305c4859ccdd727f4594
Saahil Bhavsar <saahil_bhavsar@outlook.com>
2022-07-04 15:12:48 +0530

    Updates install script

--- install.sh
+++ install.sh
@@ -4,15 +4,14 @@ echo "Starting installation..."
 
 pip install -r requirements.txt
 
-sudo mkdir -p /opt/autorepo
-sudo chown -R $USER:$USER /opt/autorepo
+sudo mkdir -p /opt/autorepo && sudo chown -R $USER:$USER /opt/autorepo
 
 mkdir -p $HOME/.config/autorepo
 
-sudo cp -r ./utils /opt/autorepo
-sudo cp ./main.py /opt/autorepo/repo
-sudo chmod +x /opt/autorepo/repo
+sudo cp -r ./utils /opt/autorepo/
+sudo cp ./main.py /opt/autorepo/repo && sudo chmod +x /opt/autorepo/repo
+sudo cp ./uninstall.sh /opt/autorepo/uninstall.sh && sudo chmod +x /opt/autorepo/uninstall.sh
 
-sudo ln -s /opt/autorepo/repo /usr/local/bin/repo
+[ ! -f /usr/local/bin/repo ] && sudo ln -s /opt/autorepo/repo /usr/local/bin/repo
 
 echo "Installation completed successfully!"
