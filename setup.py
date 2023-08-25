98dafa361625e01ad900fdace715901f012b89be
Saahil Bhavsar <saahil_bhavsar@outlook.com>
2023-08-25 09:40:20 +0530

    Updates to v0.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="autorepo",
-    version="0.1.1",
+    version="0.1.2",
     description="A simple CLI tool for managing Github Repositories",
     long_description=long_description,
     long_description_content_type="text/markdown",
